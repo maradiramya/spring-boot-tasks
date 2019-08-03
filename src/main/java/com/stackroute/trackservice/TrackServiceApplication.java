a130633487c1ad9436218f5069866e61acee90f5
ramya.p <ramya.p@cgi.com>
2019-08-03 09:24:14 +0530

    track-service-lombok task is completed

--- src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
+++ src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
@@ -1,5 +1,6 @@
 package com.stackroute.trackservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -7,9 +8,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 public class TrackServiceApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(TrackServiceApplication.class, args);
-	}
+        SpringApplication.run(TrackServiceApplication.class, args);
+    }
 
 }
--- src/main/java/com/stackroute/trackservice/domain/Track.java
+++ src/main/java/com/stackroute/trackservice/domain/Track.java
@@ -1,8 +1,16 @@
 package com.stackroute.trackservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity(name = "track")
 public class Track {
     @Id
@@ -10,45 +18,45 @@ public class Track {
     private String name;
     private String comments;
 
-    public Track() {
-    }
+//    public Track() {
+//    }
+//
+//    public Track(int id, String name, String comments) {
+//        this.id = id;
+//        this.name = name;
+//        this.comments = comments;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getComments() {
+//        return comments;
+//    }
+//
+//    public void setComments(String comments) {
+//        this.comments = comments;
+//    }
 
-    public Track(int id, String name, String comments) {
-        this.id = id;
-        this.name = name;
-        this.comments = comments;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comments='" + comments + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Track{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", comments='" + comments + '\'' +
+//                '}';
+//    }
 }
