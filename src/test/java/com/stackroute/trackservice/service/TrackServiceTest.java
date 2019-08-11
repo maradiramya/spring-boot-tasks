293ddd5f207a43ed3964b3ef7332ae42217b4984
maradiramya <52036995+maradiramya@users.noreply.github.com>
2019-08-11 23:03:19 +0530

    Update TrackServiceTest.java

--- src/test/java/com/stackroute/trackservice/service/TrackServiceTest.java
+++ src/test/java/com/stackroute/trackservice/service/TrackServiceTest.java
@@ -19,13 +19,13 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 public class TrackServiceTest {
-    Track track;
+   private Track track;
     //Create a mock for TrackRepository
     @Mock
-    TrackRepository trackRepository;
+   private TrackRepository trackRepository;
     //Inject the mocks as dependencies into TrackServiceImpl
     @InjectMocks
-    TrackServiceImpl trackService;
+   private TrackServiceImpl trackService;
     List<Track> list = null;
 
     @Before
@@ -39,10 +39,13 @@ public class TrackServiceTest {
         list = new ArrayList<>();
         list.add(track);
     }
-
-
+    @After
+    public void tearDown(){
+        track=null;
+        list=null;
+    }
     @Test
-    public void saveTrackTestSuccess() throws TrackAlreadyExistsException {
+    public void givenTrackDetailsShouldReturnSavedTrack() throws TrackAlreadyExistsException {
         when(trackRepository.save((Track) any())).thenReturn(track);
         Track savedTrack = trackService.saveTrack(track);
         Assert.assertEquals(track, savedTrack);
@@ -51,55 +54,67 @@ public class TrackServiceTest {
     }
 
     @Test(expected = TrackAlreadyExistsException.class)
-    public void saveTrackTestFailure() throws TrackAlreadyExistsException {
+    public void givenTrackDetailsShouldNotReturnSavedTrack() throws TrackAlreadyExistsException {
         trackRepository.save(track);
         when(trackRepository.existsById((track.getId()))).thenReturn(true);
         Track savedTrack = trackService.saveTrack(track);
         Assert.assertNotEquals(track, savedTrack);
-
-    }
+        //verify here verifies that trackRepository save method is only called once
+        verify(trackRepository, times(1)).save(track);
+   }
 
     @Test
-    public void getAllTrack() throws Exception {
+    public void givenTrackShouldReturnListOfSavedTrack() throws Exception {
         trackRepository.save(track);
         //stubbing the mock to return specific data
         when(trackRepository.findAll()).thenReturn(list);
         List<Track> tracklist = trackService.getAllTrack();
         Assert.assertEquals(list, tracklist);
+         //verify here verifies that trackRepository findAll method is called twice
+        verify(trackRepository, times(2)).findAll();
     }
 
     @Test
-    public void findByName() throws TrackNotFoundException {
+    public void givenNameShouldReturnTrackFromListOfTrack() throws TrackNotFoundException {
         when(trackRepository.findByName(any())).thenReturn((list));
         List<Track> fetchTrack = trackService.findByName("ramya");
         Assert.assertEquals(list, fetchTrack);
+       //verify here verifies that trackRepository findByName method is called twice
+        verify(trackRepository, times(2)).findByName("ramya");
+
 
     }
 
     @Test
-    public void getById() throws TrackNotFoundException {
+    public void givenTrackIdShouldReturnTrackOfGivenId() throws TrackNotFoundException {
         trackRepository.save(track);
         when(trackRepository.existsById(10)).thenReturn(true);
         when(trackRepository.findById(10)).thenReturn((track));
         Track fetchTrackById = trackService.getById(10);
         Assert.assertEquals(track, fetchTrackById);
+        //verify here verifies that trackRepository findById method is only called once
+        verify(trackRepository, times(1)).findById(10);
     }
 
     @Test
-    public void deleteTrackById() throws TrackNotFoundException {
+    public void givenTrackIdShouldReturnDeletedTrack() throws TrackNotFoundException {
 
         when(trackRepository.existsById(10)).thenReturn(true);
         when(trackRepository.findById(10)).thenReturn(track);
         Track deleteTrackById = trackService.getById(10);
         Assert.assertEquals(track, deleteTrackById);
+         //verify here verifies that trackRepository findById method is only called once
+        verify(trackRepository, times(1)).findById(10);
     }
 
     @Test
-    public void updateTrackById() throws TrackNotFoundException, TrackAlreadyExistsException {
+    public void givenTrackIdShouldReturnUpdatedTrack() throws TrackNotFoundException, TrackAlreadyExistsException {
         when(trackRepository.existsById(101)).thenReturn(true);
         when(trackRepository.save(track)).thenReturn(track);
         Track updateTrackById = trackService.saveTrack(track);
         Assert.assertEquals(track, updateTrackById);
+         //verify here verifies that trackRepository save method is only called once
+        verify(trackRepository, times(1)).save(track);
     }
 
 }
