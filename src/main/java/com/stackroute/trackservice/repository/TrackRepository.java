305253a1f768ae438c97da1a197e08c5bf50d260
maradiramya <52036995+maradiramya@users.noreply.github.com>
2019-08-12 00:35:59 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/trackservice/repository/TrackRepository.java
+++ src/main/java/com/stackroute/trackservice/repository/TrackRepository.java
@@ -9,11 +9,11 @@ import java.util.List;
 
 @Repository
 public interface TrackRepository extends MongoRepository<Track, Integer> {
-    //is used for defining custom queries in spring data jpa
-    //creation of custom query using name
-//    @Query(value = "select * from Track t where t.name=?")
+
     public List<Track> findByName(String name);
 
     public Track findById(int id);
+    public Track updateTrack(Track track);
+    public Track deleteTrackById(int id);
 
 }
